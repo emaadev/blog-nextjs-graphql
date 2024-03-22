f78060189aa4b34c31b03c60feced252b79f0b25
EmanuelNu <emanuel.nu02@gmail.com>
2024-03-22 08:38:52 -0300

    Changes: Modifyng styles for responsiveness

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,23 +5,13 @@ const Navbar = () => {
   return (
     <nav className="navbar-container">
       <Link href="/" className="logo">
-        <span>{"{"}</span> <span>{"}"}</span> The Big Code Theory.
+        {"{"}
+        {"}"} The Code Theory.
       </Link>
 
-      <div className="navbar-links">
-        <Link href="#allPosts" className="logo">
-          See All Posts
-        </Link>
-
-        {/* <Link
-          href="https://emanuel-nunez.vercel.app"
-          target="_blank"
-          rel="noreferrer"
-          className="logo"
-        >
-          Portfolio
-        </Link> */}
-      </div>
+      <Link href="#allPosts" className="logo">
+        See All Posts
+      </Link>
     </nav>
   );
 };
